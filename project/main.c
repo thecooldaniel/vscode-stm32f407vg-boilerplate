207ebf565fdc007e1fd7438cee24c062f602e474
Daniel Owens <kyoji1@gmail.com>
2021-05-04 23:23:02 -0400

    Use pqm4 'hal' init library by default

--- project/main.c
+++ project/main.c
@@ -1,14 +1,11 @@
-#include "include/stm32wrapper.h"
-#include <stdio.h>
+#include "include/hal.h"
 
 extern uint32_t SignedMod(int32_t number, uint32_t divisor);
 extern uint32_t UnsignedMod(uint32_t number, uint32_t divisor);
 
 int main(void)
 {
-    clock_setup();
-    gpio_setup();
-    usart_setup(115200);
+    hal_setup(CLOCK_FAST);
 
     uint32_t m = SignedMod(-10, 3);
     uint32_t m2 = UnsignedMod(538, 7);
