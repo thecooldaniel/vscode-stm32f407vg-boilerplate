ff6d849072e53c2051b390509dab4acb68634f64
Daniel Owens <kyoji1@gmail.com>
2021-12-21 19:13:51 -0500

    Add semihosting. Default debug now ext/semihosting

--- project/main.c
+++ project/main.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdint.h>
 #include "include/hal.h"
 
 extern uint32_t SignedMod(int32_t number, uint32_t divisor);
@@ -5,10 +7,13 @@ extern uint32_t UnsignedMod(uint32_t number, uint32_t divisor);
 
 int main(void)
 {
-    hal_setup(CLOCK_FAST);
+    hal_setup(CLOCK_BENCHMARK);
 
     uint32_t m = SignedMod(-10, 3);
     uint32_t m2 = UnsignedMod(538, 7);
+
+    // You must use the launch target OpenOCD External/Semihosting for the below to work
+    printf("Hello from semihosting!\n");
 
     while(1);
 
